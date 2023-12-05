5113a9b551eadf27220e7a84100f2e5c62454faf
mwalli1149 <114249650+mwalli1149@users.noreply.github.com>
2023-12-05 13:29:35 +0000

    Working On homepage

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,29 +1,23 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 function Navbar() {
   return (
     <nav className="Navbar-1">
-        <div className='Navbar-2-1'>
-            <div className='Navbar-3-1'>
-                <Link href='/'>
-                Home
-                </Link>
-            </div>
+      <div className="Navbar-2-1">
+        <div className="Navbar-3-1">
+          <Link href="/">Home</Link>
         </div>
-        <div className='Navbar-2-2'>
-            <div className='Navbar-3-2'>
-                <Link href='/log-in'>
-                    Log In
-                </Link>
-            </div>
-            <div className='Navbar-3-2'>
-                <Link href='/sign-up'>
-                    Sign Up
-                </Link>
-            </div>
+      </div>
+      <div className="Navbar-2-2">
+        <div className="Navbar-3-2">
+          <Link href="/log-in">Log In</Link>
         </div>
+        <div className="Navbar-3-2">
+          <Link href="/sign-up">Sign Up</Link>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,17 @@ import AuthButton from "./components/auth-button";
 export default function Home() {
   return (
     <main>
-      Home
+      <header>Home</header>
+      <div className="home">
+        <h1 className="home">Instructions</h1>
+        <ul>
+          <li>1.Sign In or Sign Up</li>
+          <li>
+            2.Navagate to the cattagory that corasponds to the class you are
+            looking for
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
