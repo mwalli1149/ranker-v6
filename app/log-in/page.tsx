74d7fa0bc231b0d26663a4149a31ad5a5f03a481
mwalli1149 <114249650+mwalli1149@users.noreply.github.com>
2023-12-07 00:02:55 +0000

    working on sign in page

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,14 +1,34 @@
-//Not Ready For use experimenting 
+//Not Ready For use experimenting
 "use client";
+import { useState } from "react";
 import React from "react";
-
 const LogIn = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <label>Username:</label>
-      <input className="test" />
-      <label>Password:</label>
-      <input className="test"/>
+    <div className='sign'>
+      <div className='sign-2'>
+      <label className="sign">Username:</label>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => {
+        setUsername(e.currentTarget.value);
+        }}
+      />
+      </div>
+      <div className='sign-2'>
+      <label className="mr-3.5 text-xl text-t_color_1">Password:</label>
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => {
+        setPassword(e.currentTarget.value);
+        }}
+      />
+      </div>
+      <button>Sign In</button>
     </div>
   );
 };
